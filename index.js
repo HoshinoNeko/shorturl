24fd6eb3eed7833aa4a97a900dcd87066abfef04
Neko <21078047+HoshinoNeko@users.noreply.github.com>
2023-01-02 17:41:21 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const models = require("./models");
+const validUrl = require("valid-url");
+const router = express.Router();
+
+const app = express();
+const corsOptions = {
+    origin: '*'
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+router.get('/', (req,res) => {
+    res.sendFile(__dirname + '/view')
+});
+
+router.get(':url', async (req,res) => {
+   try {
+       let url = await models.findURL(req.params.url);
+       if(url !== null) {
+           res.redirect(url)
+       } else {
+           res.send('invalid/expired URL');
+       }
+   }
+   catch(e) {
+       console.log(e);
+       res.send('internal error contact administrator')
+   }
+});
+
+router.post('api/short', async (req,res) => {
+   try {
+       let hash = await models.storeURL(req.body.url);
+       res.send(req.hostname + '/' + hash);
+   }
+   catch(e) {
+       console.log(e);
+       res.send('error occurred while creating short URL');
+   }
+});
+
+app.use('/', router);
+
+app.listen(process.env.PORT || 3000);
+console.log(`Starting server on port ${process.env.PORT || 3000}`);
---
+++ models.js
@@ -0,0 +1,57 @@
+const shortId = require("shortid");
+const redisModule = require("redis");
+const redis = redisModule.createClient({
+    host: process.env.redisHost || "127.0.0.1",
+    port: process.env.redisPort || 6379,
+    password: process.env.redisPort || ""
+});
+
+redis.on('connect', () => {
+    console.log("Connected to redis server");
+});
+
+redis.on('ready', () =>{
+   console.log("Ready to work");
+});
+
+redis.on('error', (err) => {
+    console.log("Error occurred while connecting to redis");
+});
+
+const storeURL = (url) => {
+    return new Promise((resolve, reject) => {
+        redis.get(url, (err, reply) => {
+            if (err) {
+                return reject("error occurred during redis operation");
+            }
+            if (reply) {
+                resolve(reply);
+            } else {
+                let id = shortId.generate();
+                redis.set(id, url, 'EX', 86400);
+                redis.set(url, id, 'EX', 86400);
+                resolve(id);
+            }
+        })
+    })
+};
+
+const findURL = (url) => {
+    return new Promise((resolve, reject) => {
+        redis.get(key, (err,reply) => {
+            if (err) {
+                return reject("error occurred during redis operation");
+            }
+            if (reply === null) {
+                resolve(null);
+            } else {
+                resolve(reply);
+            }
+        })
+    })
+};
+
+module.exports = {
+    storeURL: storeURL,
+    findURL: findURL
+}
